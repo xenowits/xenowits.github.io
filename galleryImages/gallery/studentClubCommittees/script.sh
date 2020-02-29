9904fbca137b953b69a6b9e1f0c965862ea30108
xenowits <kumar.abhishek111b@gmail.com>
2020-02-29 11:05:17 +0530

    new changes

--- galleryImages/gallery/studentClubCommittees/script.sh
+++
@@ -1,8 +0,0 @@
-for f in *.jpg
-do
-	filename=$(basename -- "$f")
-	extension="${filename##*.}"
-	filename="${filename%.*}"
-	echo "$filename $extension"
-	cwebp -q 0 $f -o ../../placeholder/studentClubCommittees/$filename.webp
-done
